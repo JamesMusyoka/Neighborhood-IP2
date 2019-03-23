27810c84557ffa1f5089981c7b5af591ae19554e
JamesMusyoka <jamesmu475@gmail.com>
2019-03-23 12:41:16 +0300

    Run four tests and succefully accepts

--- neighbor/tests.py
+++ neighbor/tests.py
@@ -1,6 +1,28 @@
 from django.test import TestCase
+from .models import *
 
 class NeighborhoodTestClass(TestCase):
 
     def setUp(self):
-    
+        self.kingkong= Neighborhood(neighborhood_name = 'Kingkong', neighborhood_location = 'Westlands')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.kingkong, Neighborhood))
+
+    def test_save_method(self):
+        self.kingkong.save_neighborhood()
+        neighborhood = Neighborhood.objects.all()
+        self.assertTrue(len(neighborhood) > 0)
+
+    def test_create_neighborhood(self):
+        self.kingkong.create_neighborhood()
+        neighborhood = Neighborhood.objects.all()
+        self.assertTrue(len(neighborhood) == 0)
+
+    def test_find_neighborhood(self):
+        self.kingkong.find_neighborhood()
+        neighborhood = Neighborhood.objects.all()
+        self.assertTrue(len(neighborhood) == 0)
+
+    def test_update_neighborhood
+        
