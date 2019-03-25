b6687272b3ac11550cbead8e6fa6ec0889ff5945
JamesMusyoka <jamesmu475@gmail.com>
2019-03-25 09:03:54 +0300

    Added a create_business save test

--- neighbor/tests.py
+++ neighbor/tests.py
@@ -24,5 +24,37 @@ class NeighborhoodTestClass(TestCase):
         neighborhood = Neighborhood.objects.all()
         self.assertTrue(len(neighborhood) == 0)
 
-    def test_update_neighborhood
+    def test_update_neighborhood(self):
+        self.kingkong.update_neighborhood()
+        neighborhood = Neighborhood.objects.all()
+        self.assertFalse(len(neighborhood) > 0)
+
+    def update_occupands(self):
+        self.kingkong.update_occupands()
+        neighborhood = Neighborhood.objects.all()
+        self.assertTrue(len(neighborhood) > 0)
+
+    def test_delete_neighborhood(self):
+        self.kingkong.delete_neighborhood()
+        neighborhood = Neighborhood.objects.all()
+        self.assertTrue(len(neighborhood) == 0)
         
+
+class BusinessTestClasss(TestCase):
+
+    def setUp(self):
+        self.newgeneration= Business(business_name = 'Newgeneration')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.newgeneration, Business))
+
+
+    def test_save_method(self):
+        self.newgeneration.save_business()
+        business = Business.objects.all()
+        self.assertTrue(len(business) > 0)
+
+    # def test_create_business(self):
+    #     self.newgeneration.create_neighborhood()
+    #     business = Business.objects.all()
+    #     self.assertTrue(len(business) == 1)
